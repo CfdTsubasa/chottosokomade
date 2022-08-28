aea4308f1e2ef37b04e30dd7de856d5c0f8cabb3
CfdTsubasa <78243094+CfdTsubasa@users.noreply.github.com>
2022-08-29 01:30:35 +0900

    Spotモデル追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  get 'spots/index'
+  get 'spots/create'
   get '/users/show/:id', to:'users#show',as:'user_show'
   # ログイン、アカウント編集後、任意のページに推移させるための記述
   devise_for :users, controllers: {
           registrations: 'users/registrations'
   }
+  resources :spots 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
---
+++ test/controllers/spots_controller_test.rb
@@ -0,0 +1,13 @@
+require "test_helper"
+
+class SpotsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get spots_index_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get spots_create_url
+    assert_response :success
+  end
+end
