477ea4c92cb9d089655a001cefed11d99b6c14d5
CfdTsubasa <78243094+CfdTsubasa@users.noreply.github.com>
2022-10-05 17:36:38 +0900

    スポット：場所名による位置検索

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_28_082012) do
+ActiveRecord::Schema.define(version: 2022_09_29_015312) do
 
   create_table "spots", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,10 @@ ActiveRecord::Schema.define(version: 2022_08_28_082012) do
     t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.json "images"
+    t.string "address"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_spots_on_user_id"
   end
 
