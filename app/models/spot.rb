b2d61ed9547963374f6317b5880133e43d0d6edb
CfdTsubasa <78243094+CfdTsubasa@users.noreply.github.com>
2022-10-05 17:34:41 +0900

    スポット：CRUD処理

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,4 +1,10 @@
 class Spot < ApplicationRecord
   belongs_to :user
   validates :user_id, presence: true
+  mount_uploaders :images, ImageUploader
+  after_validation :geocode
+  geocoded_by :address
+  def display_image
+    images.variant(resize_to_limit: [500, 500])
+  end
 end
