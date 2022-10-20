aed002756655b756bcc1bb8ffd9aff94123e70a3
CfdTsubasa <78243094+CfdTsubasa@users.noreply.github.com>
2022-10-21 01:08:04 +0900

    フォロー機能実装（ajax）

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,13 +1,24 @@
 class RelationshipsController < ApplicationController
   def create
     user = User.find(params[:followed_id])
+    @spot = Spot.find(params[:spot_id])
     current_user.follow(user)
-    redirect_to user_show_path(user)
+    @user = User.find(current_user.id)
+    respond_to do |format|
+      format.html { redirect_back(fallback_location: root_path) } # 前のページに遷移
+      format.js  # create.js.erbが呼び出される
+    end
   end
 
   def destroy
-    user = Relationship.find(params[:id]).followed
+    user = User.find(params[:followed_id])
     current_user.unfollow(user)
-    redirect_to user_show_path(user)
+    @spot = Spot.find(params[:spot_id])
+    @user = User.find(current_user.id)
+    respond_to do |format|
+      format.html { redirect_back(fallback_location: root_path) } # 前のページに遷移
+      format.js  # create.js.erbが呼び出される
+    end
+    # redirect_to user_show_path(user)
   end
 end
