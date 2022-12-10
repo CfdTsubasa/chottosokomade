fed06f06a19ff9546774f8812ea66d44dcd50284
CfdTsubasa <78243094+CfdTsubasa@users.noreply.github.com>
2022-12-11 01:23:26 +0900

    ストレージ設定変更

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,15 +4,8 @@ class ImageUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  # storage :file
+  storage :file
   # storage :fog
-  if Rails.env.production?
-    storage :fog
-  elsif Rails.env.test?
-    storage :file
-  else
-    storage :file
-  end
 
 
   # Override the directory where uploaded files will be stored.
