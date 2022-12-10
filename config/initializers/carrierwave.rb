226fd2a8c995702bee74025ab7da659ad0b491ad
CfdTsubasa <78243094+CfdTsubasa@users.noreply.github.com>
2022-12-10 23:43:39 +0900

    公開状況設定(config.fog_public = false→true)

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,7 +6,7 @@ CarrierWave.configure do |config|
   if Rails.env.production? # 本番環境の場合はS3へアップロード
     config.fog_provider = 'fog/aws'
     config.fog_directory  = 'tsubasaportfolio' # バケット名
-    config.fog_public = false
+    config.fog_public = true
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: ENV['S3_ACCESS_KEY_ID'], # アクセスキー
